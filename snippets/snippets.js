229292681e079c7d5ea9c08f0498ac2960c15f22
Cody Brunner <rockchalkwushock@icloud.com>
2016-09-30 09:29:38 -0500

    V1.2: Added code to snippets.js, ran different method to verify they worked.

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -1,12 +1,105 @@
 var MongoClient = require('mongodb').MongoClient;
 
-
-MongoClient.connect('mongodb://localhost/', function(err, db) {
+MongoClient.connect('mongodb://localhost/snippets', function(err, db) {
     if (err) {
         console.error(err);
         db.close();
         return;
     }
     console.log('Connected to MongoDB database');
-    db.close();
+
+    var collection = db.collection('snippets');
+
+    var create = function(name, content) {
+        var snippet = {
+            name: name,
+            content: content
+        };
+        collection.insert(snippet, function(err, result) {
+            if (err) {
+                console.error("Could not create snippet", name);
+                db.close();
+                return;
+            }
+            console.log("Created snippet", name);
+            db.close();
+        });
+    };
+
+    var read = function(name) {
+      // query is optional but look at this like your validation check
+      // to confirm we are querying the exact data we want.
+        var query = {
+            name: name
+            // name: {$eq: name}  will check that name = passed name.
+        };
+        // FindOne will return a single object.
+        collection.findOne(query, function(err, snippet) {
+            if (!snippet || err) {
+                console.error("Could not read snippet", name);
+                db.close();
+                return;
+            }
+            console.log("Read snippet", snippet.name);
+            console.log(snippet.content);
+            db.close();
+        });
+    };
+
+    var update = function(name, content) {
+        var query = {
+            name: name
+        };
+
+        var update = {
+            $set: { // $set: is an update operator find more at:
+              // https://docs.mongodb.com/manual/reference/operator/update/
+                content: content
+            }
+        };
+
+        collection.findAndModify(query, null, update, function(err, result) {
+            var snippet = result.value;
+            if (!snippet || err) {
+                console.error("Could not update snippet", name);
+                db.close();
+                return;
+            }
+            console.log("Updated snippet", snippet.name);
+            db.close();
+        });
+    };
+
+    var del = function(name, content) {
+        var query = {
+            name: name
+        };
+        collection.findAndRemove(query, function(err, result) {
+            var snippet = result.value;
+            if (!snippet || err) {
+                console.error("Could not delete snippet", name);
+                db.close();
+                return;
+            }
+            console.log("Deleted snippet", snippet.name);
+            db.close();
+        });
+    };
+
+    var main = function() {
+        if (process.argv[2] == 'create') {
+            create(process.argv[3], process.argv[4]);
+        } else if (process.argv[2] == 'read') {
+            read(process.argv[3]);
+        } else if (process.argv[2] == 'update') {
+            update(process.argv[3], process.argv[4]);
+        } else if (process.argv[2] == 'delete') {
+            del(process.argv[3]);
+        } else {
+            console.error('Command not recognized');
+            db.close();
+        }
+    };
+
+    main();
 });
